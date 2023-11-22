d5f51b577b72a9d6423f3e62a257d8931c766205
vmtuan12 <minhtuan3154@gmail.com>
2023-11-22 11:36:15 +0700

    init clientside template

---
+++ dashboard_template/README.md
@@ -0,0 +1,24 @@
+# This is a template built for Dashboard website
+
+This template is built with Vuejs, Vuerouter and Tailwindcss.
+
+You can find available components in the components folder. Every single component has comments that will instruct you how to use it properly.
+
+You can add a route to a new page in index.js in router folder.
+
+## Installation
+
+Clone this into your computer
+
+To install prerequisites, use
+```bash
+yarn install
+```
+
+To run, use
+```bash
+yarn dev
+```
+
+## Status
+This template is still in development.
---
+++ dashboard_template/src/components/UI/Navbar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import NavbarButton from './NavbarComponent/NavbarButton.vue';
+
+const emit = defineEmits(['handleBarSize']);
+const props = defineProps(['smallbar']);
+</script>
+
+<template>
+    <div 
+        id="sidebar" 
+        class="max-h-screen sticky top-0 bg-[#52a1f5] sm:border-l-8 border-solid border-[#52a1f5] transition-all duration-500 overflow-hidden" 
+        :class="smallbar ? 'w-16 max-sm:w-0' : 'w-60 max-sm:w-full'"
+    >
+        <div class="w-full flex justify-end p-4">
+            <svg 
+                @click="$emit('handleBarSize')" 
+                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" 
+                class="w-6 h-6 relative z-10 cursor-pointer text-white transition-transform duration-1000 ease-in-out"
+                :class="smallbar ? '[transform:rotateY(180deg)]' : ''"
+            >
+                <path fill-rule="evenodd" d="M13.28 3.97a.75.75 0 010 1.06L6.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0zm6 0a.75.75 0 010 1.06L12.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <ul 
+            class="relative top-0 left-0 w-full flex flex-col"
+        >
+            <NavbarButton to="/" content="Home">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7">
+                    <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                    <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+                </svg>
+            </NavbarButton>
+            <NavbarButton to="/work" content="Work">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7">
+                    <path fill-rule="evenodd" d="M7.5 5.25a3 3 0 013-3h3a3 3 0 013 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0112 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 017.5 5.455V5.25zm7.5 0v.09a49.488 49.488 0 00-6 0v-.09a1.5 1.5 0 011.5-1.5h3a1.5 1.5 0 011.5 1.5zm-3 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
+                    <path d="M3 18.4v-2.796a4.3 4.3 0 00.713.31A26.226 26.226 0 0012 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 01-6.477-.427C4.047 21.128 3 19.852 3 18.4z" />
+                </svg>
+            </NavbarButton>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    ::-webkit-scrollbar {
+        display: none;
+    }
+</style>
